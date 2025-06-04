571dbbc9faa38d86483b25db913bee9f2814c2cf
Mohannad A. Hassan <mohannad.hassan.90@gmail.com>
2025-06-04 11:34:06 +0300

    Make remoteID internal in Bookmark

--- persistence/src/commonMain/kotlin/com/quran/shared/persistence/model/Bookmark.kt
+++ persistence/src/commonMain/kotlin/com/quran/shared/persistence/model/Bookmark.kt
@@ -1,19 +1,19 @@
 package com.quran.shared.persistence.model
 
 sealed class Bookmark {
-    abstract val remoteId: String?
     abstract val lastUpdated: Long
+    internal abstract val remoteId: String?
 
     data class PageBookmark(
         val page: Int,
-        override val remoteId: String? = null,
-        override val lastUpdated: Long
+        override val lastUpdated: Long,
+        override val remoteId: String? = null
     ) : Bookmark()
 
     data class AyahBookmark(
         val sura: Int,
         val ayah: Int,
-        override val remoteId: String? = null,
-        override val lastUpdated: Long
+        override val lastUpdated: Long,
+        override val remoteId: String? = null
     ) : Bookmark()
 }
