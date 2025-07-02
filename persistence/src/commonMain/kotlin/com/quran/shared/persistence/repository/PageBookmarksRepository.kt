0b0525f4b2dfc7e9418ae4874687dbd68d4e5a21
Mohannad A. Hassan <mohannad.hassan.90@gmail.com>
2025-07-02 12:22:31 +0300

    Add documentation

--- persistence/src/commonMain/kotlin/com/quran/shared/persistence/repository/PageBookmarksRepository.kt
+++ persistence/src/commonMain/kotlin/com/quran/shared/persistence/repository/PageBookmarksRepository.kt
@@ -19,11 +19,12 @@ interface PageBookmarksSynchronizationRepository {
      * This method should be called after the remote server has confirmed the changes.
      *
      * @param updatesToPersist List of bookmarks with their remote IDs and mutation states to be
-     * persisted.
+     * persisted. These must have a remoteID setup. Make sure to call
+     * `PageBookmarkMutation.createRemoteMutation`.
      * @param localMutationsToClear List of local mutations to be cleared. An item of this list
      * denotes either a mutation that was committed remotely, or a mutation that overridden. If it
      * was committed, a counterpart is expected in `updatesToPersists` to persist it as a remote
-     * bookmark.
+     * bookmark. These must be input from the list returned by `fetchMutatedBookmarks`.
      */
     suspend fun applyRemoteChanges(updatesToPersist: List<PageBookmarkMutation>,
                                    localMutationsToClear: List<PageBookmarkMutation>)
