9fa10741adde1c509cb08e81cacefe73c430a807
Mohannad A. Hassan <mohannad.hassan.90@gmail.com>
2025-07-04 16:27:29 +0300

    Define a first draft of the SynchronizationClient

--- syncengine/src/commonMain/kotlin/com/quran/shared/syncengine/SynchronizationClient.kt
+++ syncengine/src/commonMain/kotlin/com/quran/shared/syncengine/SynchronizationClient.kt
@@ -1,5 +1,38 @@
 package com.quran.shared.syncengine
 
+data class PageBookmark(val id: String, val page: Int, val lastModified: Long)
+
+enum class Mutation {
+    CREATED,
+    DELETED,
+    MODIFIED
+}
+
+class ModelMutation<Model>(val model: Model, val modelID: String, val mutation: Mutation)
+
+interface LocalMutationsFetcher<Model> {
+    // TODO: We must allow to input the local IDs, but we shouldn't let that leak into
+    // the reported remoted mutations.
+    suspend fun fetchLocalMutations(token: Long): List<ModelMutation<Model>>
+}
+
+interface ResultNotifier<Model> {
+    suspend fun syncResult(
+        newToken: Long,
+        newRemoteMutations: List<ModelMutation<Model>>,
+        processedLocalMutations: List<ModelMutation<Model>>
+    )
+}
+
+// This will be duplicated per each model type (or generalized), as defined by the BE.
+class PageBookmarksSynchronizationConfigurations(
+    // Probably, add configurations to enable bookmark types.
+    val localMutationsFetcher: suspend (Long) -> List<ModelMutation<PageBookmark>>,
+    val resultNotifier: suspend (List<ModelMutation<PageBookmark>>, List<ModelMutation<PageBookmark>>, Long) -> Unit // Need to deliver back the local mutations
+)
+
 interface SynchronizationClient {
-    // TODO: Add synchronization methods
-} 
+    fun localDataUpdated()
+    // Or move that to a builder.
+    fun setBookmarksConfigurations(configurations: PageBookmarksSynchronizationConfigurations)
+}
