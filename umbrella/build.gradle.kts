afc91a3f62cda87bdfbab0f361d388008ad1a881
Mohannad A. Hassan <mohannad.hassan.90@gmail.com>
2025-09-15 05:30:37 +0300

    Fix cacche issue of running XCFramework of umbrella

--- umbrella/build.gradle.kts
+++ umbrella/build.gradle.kts
@@ -1,3 +1,7 @@
+import org.gradle.api.tasks.TaskAction
+import org.gradle.process.ExecOperations
+import javax.inject.Inject
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.vanniktech.maven.publish)
@@ -29,22 +33,47 @@ kotlin {
 }
 
 // Task to create XCFramework
-tasks.register("createXCFramework") {
-    dependsOn("linkReleaseFrameworkIosArm64")
-    dependsOn("linkReleaseFrameworkIosSimulatorArm64")
-    
-    doLast {
-        val xcframeworkDir = file("build/XCFrameworks/release")
-        xcframeworkDir.mkdirs()
-        
-        exec {
+abstract class CreateXCFrameworkTask : DefaultTask() {
+    @get:InputDirectory
+    abstract val arm64Framework: DirectoryProperty
+
+    @get:InputDirectory
+    abstract val simulatorFramework: DirectoryProperty
+
+    @get:OutputDirectory
+    abstract val outputDirectory: DirectoryProperty
+
+    @get:Inject
+    abstract val execOperations: ExecOperations
+
+    @TaskAction
+    fun createXCFramework() {
+        val outputFramework = outputDirectory.file("QuranSync.xcframework").get().asFile
+
+        // Clean existing XCFramework if it exists
+        if (outputFramework.exists()) {
+            outputFramework.deleteRecursively()
+        }
+
+        outputDirectory.get().asFile.mkdirs()
+
+        execOperations.exec {
             commandLine("xcodebuild", "-create-xcframework",
-                "-framework", "build/bin/iosArm64/releaseFramework/QuranSyncUmbrella.framework",
-                "-framework", "build/bin/iosSimulatorArm64/releaseFramework/QuranSyncUmbrella.framework",
-                "-output", "build/XCFrameworks/release/QuranSyncUmbrella.xcframework"
+                "-framework", arm64Framework.get().asFile.absolutePath,
+                "-framework", simulatorFramework.get().asFile.absolutePath,
+                "-output", outputFramework.absolutePath
             )
         }
     }
+}
+
+tasks.register<CreateXCFrameworkTask>("createXCFramework") {
+    dependsOn("linkReleaseFrameworkIosArm64")
+    dependsOn("linkReleaseFrameworkIosSimulatorArm64")
+
+    arm64Framework.set(layout.buildDirectory.dir("bin/iosArm64/releaseFramework/QuranSync.framework"))
+    simulatorFramework.set(layout.buildDirectory.dir("bin/iosSimulatorArm64/releaseFramework/QuranSync.framework"))
+    outputDirectory.set(layout.buildDirectory.dir("XCFrameworks/release"))
 }
 
 mavenPublishing {
