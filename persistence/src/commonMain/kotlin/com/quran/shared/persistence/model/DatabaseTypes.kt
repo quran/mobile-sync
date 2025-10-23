4b7957b406fd9cbb617bb27d3d8f40e485559a47
Ahmed El-Helw <ahmedre@gmail.com>
2025-10-23 16:08:02 +0400

    Use kmp native coroutines and clean up visibility
    This PR cleans up the visibility of various Kotlin functions to avoid
    certain methods (ex the specific underlying database methods) from being
    called from Android/Swift apps. It also adds the dependency and
    annotations for kmp-nativecoroutines.

--- persistence/src/commonMain/kotlin/com/quran/shared/persistence/model/DatabaseTypes.kt
+++ persistence/src/commonMain/kotlin/com/quran/shared/persistence/model/DatabaseTypes.kt
@@ -2,4 +2,4 @@ package com.quran.shared.persistence.model
 
 import com.quran.shared.persistence.Page_bookmark
 
-typealias DatabasePageBookmark = Page_bookmark
+internal typealias DatabasePageBookmark = Page_bookmark
