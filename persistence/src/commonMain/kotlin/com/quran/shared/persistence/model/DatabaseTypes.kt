cd9cf78125e884cb6d9fbaf990fb9e1e4fb2541c
Ahmed El-Helw <ahmedre@gmail.com>
2025-12-17 01:39:49 +0400

    Add initial scaffolding for ayah bookmarks

--- persistence/src/commonMain/kotlin/com/quran/shared/persistence/model/DatabaseTypes.kt
+++ persistence/src/commonMain/kotlin/com/quran/shared/persistence/model/DatabaseTypes.kt
@@ -1,5 +1,7 @@
 package com.quran.shared.persistence.model
 
+import com.quran.shared.persistence.Ayah_bookmark
 import com.quran.shared.persistence.Page_bookmark
 
 internal typealias DatabasePageBookmark = Page_bookmark
+internal typealias DatabaseAyahBookmark = Ayah_bookmark
