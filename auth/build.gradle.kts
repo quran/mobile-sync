5d77bece07fea22b0cf96eba9b0232bd3b915aad
Ahmed Nabil <ahmed.mdeveloper@gmail.com>
2026-01-15 23:50:25 +0200

    update AuthenticationManager.kt with correct url scheme, and use platform-agnostic functions without expect/actual need

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -26,6 +26,7 @@ kotlin {
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.json)
+            implementation(libs.sha2)
         }
 
         commonTest.dependencies {
