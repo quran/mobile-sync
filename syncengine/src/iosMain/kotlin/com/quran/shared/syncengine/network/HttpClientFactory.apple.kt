b492a3ad59f671c7c05c57ddf3a15a0e72a02ef7
Mohamed Afifi <mohamede1945@gmail.com>
2026-07-28 21:39:05 -0400

    feat: add configurable iOS HTTP logging
    Route Ktor logs through Kermit, support runtime log levels, and redact authentication and cookie headers.

--- syncengine/src/iosMain/kotlin/com/quran/shared/syncengine/network/HttpClientFactory.apple.kt
+++ syncengine/src/iosMain/kotlin/com/quran/shared/syncengine/network/HttpClientFactory.apple.kt
@@ -1,12 +1,19 @@
 package com.quran.shared.syncengine.network
 
+import co.touchlab.kermit.Logger as KermitLogger
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.darwin.Darwin
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger as KtorLogger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.HttpHeaders
 import kotlinx.serialization.json.Json
 import io.ktor.serialization.kotlinx.json.json
+import platform.Foundation.NSProcessInfo
+
+private const val HTTP_LOG_LEVEL_ENVIRONMENT_KEY = "MOBILE_SYNC_HTTP_LOG_LEVEL"
+private val httpLogger = KermitLogger.withTag("KtorHTTP")
 
 actual object HttpClientFactory {
     actual fun createHttpClient(): HttpClient {
@@ -15,8 +22,29 @@ actual object HttpClientFactory {
                 json(Json { explicitNulls = false })
             }
             install(Logging) {
-                level = LogLevel.INFO
+                logger = object : KtorLogger {
+                    override fun log(message: String) {
+                        httpLogger.d { message }
+                    }
+                }
+                level = configuredHttpLogLevel()
+                sanitizeHeader { header ->
+                    header.equals(HttpHeaders.Authorization, ignoreCase = true) ||
+                        header.equals(HttpHeaders.Cookie, ignoreCase = true) ||
+                        header.equals(HttpHeaders.SetCookie, ignoreCase = true)
+                }
             }
         }
     }
-} 
+}
+
+private fun configuredHttpLogLevel(): LogLevel {
+    val configuredLevel = NSProcessInfo.processInfo.environment[HTTP_LOG_LEVEL_ENVIRONMENT_KEY] as? String
+    return when (configuredLevel?.uppercase()) {
+        "ALL" -> LogLevel.ALL
+        "BODY" -> LogLevel.BODY
+        "HEADERS" -> LogLevel.HEADERS
+        "NONE" -> LogLevel.NONE
+        else -> LogLevel.INFO
+    }
+}
